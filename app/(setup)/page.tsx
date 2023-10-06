cd2e87d5642a1208dbb716281572b2361d0c9f4b
hayato94087 <hayato94087@gmail.com>
2023-10-06 10:56:22 +0900

    Prisma & DB Setup

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,31 @@
+import {redirect} from 'next/navigation'
+import {FC} from 'react'
+import { initialProfile } from '@/lib/initial-profile';
+import { db } from '@/lib/db';
+
+interface SetupPageProps {}
+
+const SetupPage: FC<SetupPageProps> = async ({}) => {
+  const profile = await initialProfile();
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id
+        }
+      }
+    }
+  });
+
+  if(server) {
+    return redirect(`/servers/${server.id}`)
+  }
+
+  return (
+    <div className=''>
+      Create a Server
+    </div>
+  );
+};
+
+export default SetupPage;
