0728f8b7fb9769d2bd986468bc6c953e0e0d2d2c
hayato94087 <hayato94087@gmail.com>
2023-10-07 05:19:00 +0900

    Navigation Sidebar

---
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -0,0 +1,13 @@
+import {FC} from 'react'
+
+interface ServerIdPageProps {}
+
+const ServerIdPage: FC<ServerIdPageProps> = ({}) => {
+  return (
+    <div className=''>
+      <div>ServerIdPage</div>
+    </div>
+  );
+};
+
+export default ServerIdPage;
