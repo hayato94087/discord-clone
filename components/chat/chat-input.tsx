b28bd18f1c2fc6786a3f6137a04353ee3d732950
hayato94087 <hayato94087@gmail.com>
2023-10-09 22:36:45 +0900

    Message Attachment

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import axios from "axios";
-import qs from "query-string"
+import qs from "query-string";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { FC } from "react";
 import { useForm } from "react-hook-form";
@@ -10,6 +10,7 @@ import * as z from "zod";
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Plus, Smile } from "lucide-react";
 import { Input } from "@/components/ui/input";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ChatInputProps {
   apiUrl: string;
@@ -23,6 +24,7 @@ const formSchema = z.object({
 });
 
 const ChatInput: FC<ChatInputProps> = ({ apiUrl, query, name, type }) => {
+  const { onOpen } = useModal();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -56,7 +58,7 @@ const ChatInput: FC<ChatInputProps> = ({ apiUrl, query, name, type }) => {
                 <div className="relative p-4 pb-6">
                   <button
                     type="button"
-                    onClick={() => {}}
+                    onClick={() => onOpen("messageFile", { apiUrl, query })}
                     className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center"
                   >
                     <Plus className="text-white dark:text-[#]" />
@@ -64,7 +66,9 @@ const ChatInput: FC<ChatInputProps> = ({ apiUrl, query, name, type }) => {
                   <Input
                     disabled={isLoading}
                     className="px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
-                    placeholder={`Message ${type ==="conversation" ? name: "#"+name}`}
+                    placeholder={`Message ${
+                      type === "conversation" ? name : "#" + name
+                    }`}
                     {...field}
                   />
                   <div className="absolute top-7 right-8">
